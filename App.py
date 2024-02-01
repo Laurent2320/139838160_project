47a11a143bc0ac9d8670170ca45f84606d35677f
= <labarthod@gmail.com>
2024-02-01 15:29:09 +0100

    Update App.py new architecture functions

--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-# App.py
 # loading libraries
 import numpy as np
 import pandas as pd
@@ -8,6 +7,35 @@ from keras.models import load_model
 import requests
 import zipfile
 import io
+
+# Scrapping and loading model
+def scrap_fct(url):
+  # Scrapping file results from FDJ URL
+  # URL of the ZIP file
+
+
+  # Send a GET request to the URL
+  response = requests.get(url)
+
+  # Check if the request was successful (status code 200)
+  if response.status_code == 200:
+    # Open a file-like object
+    zip_file = io.BytesIO(response.content)
+
+    # Extract the contents of the ZIP file
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+      # Find the CSV file within the ZIP
+      csv_file_name = zip_ref.namelist()[0]
+
+      # Read the CSV file into a DataFrame df_scraped
+      with zip_ref.open(csv_file_name) as csv_file:
+        df_scraped = pd.read_csv(csv_file, sep=';', encoding='ISO-8859-1')
+
+  return df_scraped
+
+
+
+# Builduing Features functions
 
 # frequency of a number in the last window rows
 def count_frequency(dataframe, windows):
@@ -46,12 +74,15 @@ def no_ball(data):
 
 # Function to create sequences for X and y depending on the windows value
 def create_sequences(data, length):
-    X,Y = [],[]
+    X = []
     for i in range(len(data)-length):
         X.append(data.iloc[i+1:i+length+1, 0:data.shape[1]].values)
-    for i in range(len(data)-length):
-      Y.append(data.iloc[i+length, 0:7])
-    return np.array(X),np.array(Y)
+    return np.array(X)
+
+# Prediction functions & Post Traitement function
+def add_new_grids(df_ligth, grids):
+  columns = ['boule_1',	'boule_2',	'boule_3',	'boule_4',	'boule_5',	'etoile_1',	'etoile_2']
+  return pd.concat([df_ligth, pd.DataFrame(grids, columns=columns)], ignore_index=True)
 
 def post_traitement(predic):
   array = np.clip(np.round(predic[0]).astype(int), 1, 50)
@@ -84,52 +115,38 @@ def post_traitement(predic):
   return array.tolist()
 
 
-# Scrapping file results from FDJ URL
-# URL of the ZIP file
-url = "https://media.fdj.fr/static-draws/csv/euromillions/euromillions_202002.zip"
+# Main Function
+def draws(grids_nbr, rows_windows):
+  df_scraped = scrap_fct(url = "https://media.fdj.fr/static-draws/csv/euromillions/euromillions_202002.zip")
+  # Building Features for predictions from df_scraped
+  df_ligth = df_scraped[['boule_1','boule_2','boule_3','boule_4','boule_5','etoile_1','etoile_2']]
+  # flip rows from the oldest one to the newest
+  df_ligth_rev = df_ligth[::-1].reset_index(drop=True)
+  df_extend = df_ligth_rev.copy()
+  grids = []
+  columns = ['boule_1',	'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']
+  for i in range(grids_nbr):
+    # Frequency of a number in the last window rows
+    df_extend = count_frequency(df_extend, rows_windows)
+    # Compute square difference between numbers
+    df_extend = quadra_dif(df_extend)
+    # Last time the number was drawn
+    df_extend = no_ball(df_extend)
+    df_extend = no_star(df_extend)
+    # Create sequences for X and y
+    X = create_sequences(df_extend, rows_windows)
+    predictions = model.predict(X[-1:])
+    grids.append(post_traitement(predictions))
+    df_extend = add_new_grids(df_ligth_rev, grids)
+  return grids
 
-# Send a GET request to the URL
-response = requests.get(url)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Open a file-like object
-    zip_file = io.BytesIO(response.content)
-
-    # Extract the contents of the ZIP file
-    with zipfile.ZipFile(zip_file, "r") as zip_ref:
-        # Find the CSV file within the ZIP
-        csv_file_name = zip_ref.namelist()[0]
-
-        # Read the CSV file into a DataFrame df_scraped
-        with zip_ref.open(csv_file_name) as csv_file:
-            df_scraped = pd.read_csv(csv_file, sep=';', encoding='ISO-8859-1')
+# Parameters
+grids_nbr = 3
+windows = 12
 
 # Load the model from the H5 file
 model = load_model('test_valid_test_512_64_12_0.2.h5')
 
-# Define the sequence length
-windows = 12
-
-# Building Features for predictions from df_scraped
-df_ligth = df_scraped[['boule_1','boule_2','boule_3','boule_4','boule_5','etoile_1','etoile_2']]
-
-# flip rows from the oldest one to the newest
-df_ligth = df_ligth[::-1].reset_index(drop=True)
-
-# Add frequencies columns
-df_extend = count_frequency(df_ligth, windows = windows)
-# Add column with how long the number didn't not appear
-df_extend = quadra_dif(df_extend)
-# create columns counting number of draft without this number or star
-df_extend = no_ball(df_extend)
-df_extend = no_star(df_extend)
-
-# Create sequences for X and y
-X, y = create_sequences(df_extend, windows)
-
-#data X for prediction
-predictions = model.predict(X[-1:])
-
-# Call Post traitement function
-post_traitement(predictions)
+# Call main function
+draws_pred = draws(grids_nbr, windows)
+draws_pred
